7aff26a889b612ad88cfbec67501ccbe16437b0d
Juan <Juan@Juan.lan>
2018-04-08 23:02:44 -0500

    actualizando proyecto de ejercicios

---
+++ Ejercicios_libro/src/capitulo1/ejercicio1_1.java
@@ -0,0 +1,12 @@
+//Juan Antonio Gutierrez Alvarez
+//Ing. Sistemas Computacionales
+//Capitulo 1 ejercicios
+package capitulo1;
+
+/**
+ *
+ * @author Juan
+ */
+public class ejercicio1_1 {
+    
+}
